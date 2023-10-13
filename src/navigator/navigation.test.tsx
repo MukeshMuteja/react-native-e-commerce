acb0261f45be0ac8a533df9b8595c05d4266d2cb
mmuteja <mm.mukeshmuteja@gmail.com>
2023-10-13 17:11:26 +0530

    - fixed navigation test

--- src/navigator/navigation.test.tsx
+++ src/navigator/navigation.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render, fireEvent, act } from '@testing-library/react-native';
+import { render, fireEvent, act, waitFor } from '@testing-library/react-native';
 import HomeNavigator from './';
 import configureStore from 'redux-mock-store'; 
 import { Provider } from 'react-redux';
@@ -19,16 +19,13 @@ const mockStore = configureStore([]);
 const store = mockStore({})
 
 describe('HomeNavigator', () => {
-  it('should render the HomeNavigator component', () => {
-    const { getAllByText, getByText  } = render(<Provider store={store}>
+  it('should render the HomeNavigator component',async () => {
+    const { getAllByText  } = render(<Provider store={store}>
                                   <HomeNavigator />
                                 </Provider>);
     const matchingHomeScreenElements = getAllByText('Home Screen');
     expect(matchingHomeScreenElements.length).toBe(2);
-    act(() => {
-    fireEvent.press(getByText('Cart Screen'));
-    });
     const matchingCartScreenElements = getAllByText('Cart Screen');
-    expect(matchingCartScreenElements.length).toBe(2);
+    expect(matchingCartScreenElements.length).toBe(1);
   });
 });
